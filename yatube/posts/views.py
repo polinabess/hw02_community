5443b4f7dc4adf56ad828685d4af400f8564f37c
Polina Bessogonova <girls_kiss@mail.ru>
2022-10-10 20:45:02 +0300

    исправлены ошибки

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -8,7 +8,7 @@ from .models import Post, Group
 def index(request):
     # Одна строка вместо тысячи слов на SQL:
     # в переменную posts будет сохранена выборка из 10 объектов модели Post,
-    # отсортированных по полю pub_date по убыванию 
+    # отсортированных по полю pub_date по убыванию
     # (от больших значений к меньшим)
     posts = Post.objects.order_by('-pub_date')[:10]
     # В словаре context отправляем информацию в шаблон
@@ -26,4 +26,3 @@ def group_posts(request, slug):
         'posts': posts,
     }
     return render(request, 'posts/group_list.html', context)
-
