8fb8be7cd0c20d3a48ad1921f7ea5ee69ab6393a
Polina Bessogonova <girls_kiss@mail.ru>
2022-10-14 23:02:14 +0300

    проведена миграция. добавлен отступ. переименован файл-includes

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render, get_object_or_404
 # Create your views here.
 # from django.http import HttpResponse
 from .models import Post, Group
+
+
 AMOUNT_POSTS_ON_PAGE = 10
 
 
